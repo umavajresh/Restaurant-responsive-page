6ff7bfe12a0623da23547455e232605542d0d63c
umavajresh <umavajresh9@gmail.com>
2026-06-17 15:09:01 +0530

    Updated website and fixed mobile menu

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-Let menu = document.querySelector("#menu-icon");
-Let navbar =document.querySelector(".navbar");
-menu.addEventListener("click", function (){
-    navbar.classList.toggle("active");
-});
-window.onscroll = () =>{
-    navbar.classList.remove("active");
-}
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
